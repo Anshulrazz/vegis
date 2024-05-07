0690828379c294bc53b58d49ff57b1b3d16028a1
Anshul Kumar <110094962+Anshulrazz@users.noreply.github.com>
2024-05-07 21:01:05 +0530

    Add files via upload

---
+++ src/assets/Data.js
@@ -0,0 +1,55 @@
+// import img from './'
+
+const itemCard = [
+    {
+      id: 1,
+      itemImage: './public/Fruits & Vegetables/Watermelon.png',
+      itemTitle: 'Organic Watermelon',
+      price: '198.00',
+    },
+    {
+      id: 2,
+      itemImage: './public/Fruits & Vegetables/Tomato.png',
+      itemTitle: 'Fresh Tomato',
+      price: '210.00',
+    },
+    {
+      id: 3,
+      itemImage: './public/Fruits & Vegetables/Mushrom.png',
+      itemTitle: 'Natural Mushroom',
+      price: '118.00',
+    },
+    {
+      id: 4,
+      itemImage: './public/Fruits & Vegetables/Brocilli.png',
+      itemTitle: 'Natural Broccoli',
+      price: '178.00',
+    },
+    {
+      id: 5,
+      itemImage: './public/Fruits & Vegetables/Corn.png',
+      itemTitle: 'Sweet Corn',
+      price: '218.00',
+    },
+    {
+      id: 6,
+      itemImage: './public/Fruits & Vegetables/Asparagus.png',
+      itemTitle: 'Organic Asparagus',
+      price: '118.00',
+    },
+    {
+      id: 7,
+      itemImage: './public/Fruits & Vegetables/Onion.png',
+      itemTitle: 'Organic Onion',
+      price: '248.00',
+    },
+    {
+      id: 8,
+      itemImage: './public/Fruits & Vegetables/Carrot.png',
+      itemTitle: 'Fresh Carrots',
+      price: '138.00',
+    },
+  ];
+  
+  export default itemCard;
+  
